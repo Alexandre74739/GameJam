a2134cfdbb2bfd1ac37e76dc9af61567bfa695c0
Alexandre74739 <perezalexandre430@gmail.com>
2026-02-02 20:10:22 +0100

    ajout music général + coin

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,22 +1,23 @@
 extends Area2D
 
-# On accède aux maps via le parent (Game)
 @onready var map_chaude = get_node("/root/Game/MapChaude")
 @onready var map_froide = get_node("/root/Game/MapFroide")
+
+# On ne récupère que la musique de la pièce ici
+@onready var music_coin = get_node("/root/Game/MusicCoin")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		exchange_worlds()
-		# On ne met PAS de queue_free() ici pour que la pièce reste !
+		play_coin_sound() # On lance le son spécial
 
 func exchange_worlds():
-	# 1. Échange des Z-Index des maps parentes
+	# Échange des Z-index
 	var temp_z = map_chaude.z_index
 	map_chaude.z_index = map_froide.z_index
 	map_froide.z_index = temp_z
 	
-	# 2. Mise à jour de l'état des plateformes
-	# On active celle qui a le Z-index le plus haut (0 ou plus)
+	# Mise à jour des plateformes
 	if map_chaude.z_index >= 0:
 		update_platforms(map_chaude, true)
 		update_platforms(map_froide, false)
@@ -27,12 +28,13 @@ func exchange_worlds():
 func update_platforms(map_node: Node2D, is_active: bool):
 	var platforms = map_node.get_node_or_null("Platforms")
 	if platforms:
-		# On aligne le Z-index du nœud Platforms sur celui de sa map
 		platforms.z_index = map_node.z_index
-		
-		# On active/désactive la physique (Collision)
 		platforms.process_mode = PROCESS_MODE_INHERIT if is_active else PROCESS_MODE_DISABLED
-		
-		# On rend visible ou invisible
 		if platforms is CanvasItem:
 			platforms.visible = is_active
+
+func play_coin_sound():
+	# On joue le son de la pièce sans arrêter la musique générale
+	if music_coin.playing:
+		music_coin.stop() # On recommence le son si on touche une autre pièce vite
+	music_coin.play()
