ef6d4c7134b3c3e23cb262bf24edf78191bdb9c4
Alexandre74739 <perezalexandre430@gmail.com>
2026-02-03 22:45:05 +0100

    ajout 1 kill = 2 spawn

--- scripts/wave_manager.gd
+++ scripts/wave_manager.gd
@@ -2,32 +2,34 @@ extends Node
 
 signal wave_changed(wave_number)
 
-var current_wave = 0
-var enemies_alive = 0
+var current_wave = 1
+# On n'a plus forcément besoin de "enemies_alive" pour bloquer, 
+# puisque ça spawn à l'infini selon tes morts.
 
-func start_next_wave():
-	current_wave += 1
-	enemies_alive = current_wave + 1  # Ta règle : Manche + 1
+func _ready():
+	# Petit délai pour laisser la scène charger
+	await get_tree().create_timer(1.0).timeout
 	wave_changed.emit(current_wave)
-	spawn_wave()
-
-func spawn_wave():
-	for i in range(enemies_alive):
-		spawn_enemy()
+	# On commence avec un premier ennemi pour lancer la boucle
+	spawn_enemy()
 
 func spawn_enemy():
 	var enemy_scene = load("res://scenes/pnj.tscn")
 	var enemy = enemy_scene.instantiate()
 	
-	# Il va chercher dans le groupe de points que tu viens de créer
-	var spawn_node = get_tree().current_scene.get_node("SpawnPoints")
-	var points = spawn_node.get_children()
-	var random_point = points.pick_random()
-	
-	enemy.global_position = random_point.global_position
-	get_tree().current_scene.add_child(enemy)
+	# Trouve le nœud contenant les Marker2D
+	var spawn_node = get_tree().current_scene.get_node_or_null("SpawnPoints")
+	if spawn_node:
+		var points = spawn_node.get_children()
+		if points.size() > 0:
+			var random_point = points.pick_random()
+			enemy.global_position = random_point.global_position
+			get_tree().current_scene.add_child(enemy)
 
 func enemy_died():
-	enemies_alive -= 1
-	if enemies_alive <= 0:
-		start_next_wave()
+	# Dès qu'un ennemi meurt (tué ou vide), on en crée 2
+	spawn_enemy()
+	spawn_enemy()
+	
+	# Optionnel : On augmente le compteur de score/vague tous les X morts
+	# ou on considère que chaque mort fait progresser la difficulté.
