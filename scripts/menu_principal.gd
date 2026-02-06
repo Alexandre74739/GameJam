423507b37122cfeadb5e83bfd39ef0255c07d7cb
koffiwilly <willy.koffi@outlook.fr>
2026-02-06 11:24:37 +0100

    degrader de fin remis propre

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -1,20 +1,13 @@
 extends Control
 
-# On récupère les références des nodes en haut du script pour plus de clarté
-@onready var rules_panel = $RulesPanel
-
 func _on_play_button_pressed():
 	# Le chemin vers game.tscn doit être exact (minuscules/majuscules)
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_rules_button_pressed():
-	
+	# Affiche la page des crédits/règles
 	get_tree().change_scene_to_file("res://scenes/page_crédits.tscn")
 
-func _on_back_button_pressed(): 
-	rules_panel.hide() # Cache le panneau
-	
-
-
 func _on_back_button_button_down() -> void:
+	# Quitte le jeu proprement
 	get_tree().quit()
